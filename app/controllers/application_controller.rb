083150e57ca9e445003133faa5ed73ea28d21914
Obafemi Fadairo <obafemifadairo@macbook-pro-5.alk.com>
2018-04-18 18:32:35 -0400

    Adding 'gets' and 'posts' to controller for signup and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,22 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
+
+
+  get '/' do
+  end
+
+  get '/signup' do
+  end
+
+  post '/signup' do
+  end
+
+  get '/login' do
+  end
+
+  post '/login' do
+  end
+
+
 end
